75411a67c661b829ae4b2dc67335d07e6a442b5a
man1top <162521931+man1top@users.noreply.github.com>
2024-09-20 14:46:46 +0900

    Create UserRespons.java

---
+++ src/main/java/com/example/demo/controller/UserRespons.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    private static final int USER_ID = 12345
+    
+    @GetMapping("/api/v1/user")
+    public UserResponse getUser() {
+        return new UserRespones(USER_ID);
+    }
+}
