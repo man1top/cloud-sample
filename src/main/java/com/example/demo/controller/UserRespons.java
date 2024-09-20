ca187d78049a96babb36f8cb768f67d881e14826
man1top <162521931+man1top@users.noreply.github.com>
2024-09-20 14:48:21 +0900

    Update UserRespons.java

--- src/main/java/com/example/demo/controller/UserRespons.java
+++ src/main/java/com/example/demo/controller/UserRespons.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
     private static final int USER_ID = 12345
+
     
     @GetMapping("/api/v1/user")
     public UserResponse getUser() {
