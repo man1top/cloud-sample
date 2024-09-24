5318381f050d24339e4087f7ed147cdab0e2ab9f
man1top <namgichoi@gmail.com>
2024-09-24 13:35:36 +0900

    source edit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,10 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    private static final int USER_ID = 12345
+    private static final int USER_ID = 12345;
     
     @GetMapping("/api/v1/user")
     public UserResponse getUser() {
-        return new UserRespones(USER_ID);
+        return new UserResponse(USER_ID);
+    
+    }
+
+    public static class UserResponse{
+        private int user_id;
+        
+        public UserResponse(int user_id){
+            this.user_id = user_id;
+        }
+
+        public int getUser_id(){
+            return user_id;
+        }
     }
 }
